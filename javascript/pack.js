9fa20f325ace7a3ee473365d0cae0f18004e3d4e
Phansa <blaze5545@gmail.com>
2017-05-06 16:12:00 -0400

    adding for loop to display all images in the set

--- javascript/pack.js
+++ javascript/pack.js
@@ -1,8 +1,25 @@
+//URL For Cards
+//http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=425901&type=card
+
+
 function pack()
 {
-	//Loops through all cards in the JSON file for the set Amonkhet and prints their name
+	//Loops through all cards in the JSON file for the set Amonkhet and displays their image
+	var timeout = 1000;
 	for(var card in AllSets.AKH.cards)
 	{
-		console.log(AllSets.AKH.cards[card].name)
+		//console.log(AllSets.AKH.cards[card].name);
+		var image_link = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + AllSets.AKH.cards[card].multiverseid.toString() + "&type=card";
+		doSetTimeout(image_link, timeout);
+		timeout += 1000;
 	}
+}
+
+function doSetTimeout(image_link_in, time) {
+	setTimeout(function(){image_swap(image_link_in);}, time);
+}
+
+function image_swap(image_link_in)
+{
+	document.getElementById("test").src = image_link_in;
 }
