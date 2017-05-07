c32673c4cf3243658707566b8f20d2ac4772fe7e
Phansa <blaze5545@gmail.com>
2017-05-07 13:54:21 -0400

    updating console logging, allowing looping through any set

--- javascripts/pack.js
+++ javascripts/pack.js
@@ -7,20 +7,27 @@ function loop()
 {
 	//Loops through all cards in the JSON file for the set Amonkhet and displays their image
 	var timeout = 1000;
-	for(var card in AllSets.AKH.cards)
+	var set_name = document.getElementById("SetName").value;
+	for(var set in AllSets)
 	{
-		//console.log(AllSets.AKH.cards[card].name);
-		var image_link = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + 
-		AllSets.AKH.cards[card].multiverseid.toString() + "&type=card";
-		doSetTimeout(image_link, timeout);
-		timeout += 1000;
+		if(AllSets[set].code == set_name)
+		{
+			stop_loop = false;
+			for(var card in AllSets[set].cards)
+			{
+				var image_link = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + 
+				AllSets[set].cards[card].multiverseid.toString() + "&type=card";
+				doSetTimeout(image_link, timeout);
+				timeout += 1000;
+			}
+			break;
+		}
 	}
 }
 function preprocess_set_codes()
 {
 	for(var set in AllSets)
 	{
-		//console.log(AllSets[set].code);
 		valid_set_codes.add(AllSets[set].code.toString());
 	}
 }
@@ -32,13 +39,11 @@ function process_pack(set_name)
 	{
 		if(AllSets[set].code == set_name)
 		{
-			console.log(AllSets[set].cards.length);
+			//console.log(AllSets[set].cards.length);
 
 			//Add current to preloaded sets
 			if(!(preloaded_sets.has(set_name)))
 			{
-				//preloaded_set_names.add(set_name);
-				//preloaded_sets.push(set_name);
 				var new_set = new Object();
 				var commons = [];
 				var uncommons = [];
@@ -64,9 +69,9 @@ function process_pack(set_name)
 						mythic_rares.push(card);
 					}
 				}
-				new_set.name = "AKH";
+				new_set.name = set_name;
 				new_set.commons = commons;
-				console.log("CREATING");
+				console.log("CREATING SET");
 				new_set.uncommons = uncommons;
 				new_set.rares = rares;
 				new_set.mythics = mythic_rares;
@@ -78,9 +83,9 @@ function process_pack(set_name)
 			while(PackContents.size < 11)
 			{
 				index = Math.floor(Math.random() * cur_set.commons.length);
-				console.log("COMMONS " + index.toString());
 				if(!PackContents.has(cur_set.commons[index]))
 				{
+					console.log(cur_set.commons[index].name);
 					PackContents.add(cur_set.commons[index]);
 					urls.push("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + 
 						cur_set.commons[index].multiverseid.toString() + "&type=card");
@@ -89,9 +94,9 @@ function process_pack(set_name)
 			while(PackContents.size < 14)
 			{
 				index = Math.floor(Math.random() * cur_set.uncommons.length);
-				console.log("UNCOMMONS " + index.toString());
 				if(!PackContents.has(cur_set.uncommons[index]))
 				{
+					console.log(cur_set.uncommons[index].name);
 					PackContents.add(cur_set.uncommons[index]);
 					urls.push("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + 
 						cur_set.uncommons[index].multiverseid.toString() + "&type=card");
@@ -105,15 +110,15 @@ function process_pack(set_name)
 				if(mythic_upgrade == 7)
 				{	
 					index = Math.floor(Math.random() * cur_set.mythics.length);
-					console.log("MYTHIC RARES " + index.toString());
+					console.log(cur_set.mythics[index].name);
 					PackContents.add(cur_set.mythics[index]);
 					urls.push("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + 
 					cur_set.mythics[index].multiverseid.toString() + "&type=card");
 				}
 				else
 				{
-					console.log("RARES " + index.toString());
 					index = Math.floor(Math.random() * cur_set.rares.length);
+					console.log(cur_set.rares[index].name);
 					PackContents.add(cur_set.rares[index]);
 					urls.push("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + 
 					cur_set.rares[index].multiverseid.toString() + "&type=card");
@@ -132,7 +137,6 @@ function open_pack()
 {
 	//Specify Set Name and check if valid
 	var set_name = document.getElementById("SetName").value;
-	console.log(set_name);
 	if(valid_set_codes.has(set_name))
 	{
 		if(document.getElementsByName("options")[1].checked)
@@ -155,7 +159,10 @@ function open_pack()
 	}
 }
 function doSetTimeout(image_link_in, time) {
-	setTimeout(function(){image_swap(image_link_in);}, time);
+	if(!stop_loop)
+	{
+		setTimeout(function(){image_swap(image_link_in);}, time);
+	}
 }
 
 function image_swap(image_link_in)
